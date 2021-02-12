d89ac38e7b0dfa609f07846796fbb5e0b221c91f
Anuar <anuar_daher@hotmail.com>
2021-02-12 06:18:28 -0300

    github's api frontend

---
+++ src/pages/Index.vue
@@ -0,0 +1,100 @@
+<template>
+  <Layout>
+    <h1 class="text-2xl font-bold mb-4 text-center">List of Github Users</h1>
+
+    <div class="list-of-users grid grid lg:grid-cols-5 grid-cols-2 lg:gap-4 gap-2 text-white">
+      <div
+        class="user-card bg-gradient-to-r from-gray-700 to-gray-900 rounded-lg puff-in-center "
+        v-for="user in users.data"
+        :key="user.id"
+      >
+        <div class="ml-2 font-semibold text-sm">{{ user.id }}</div>
+        <g-link :to="'/' + user.login"><div class="text-base text-center cursor-pointer hover:text-blue-400">{{ user.login }}</div></g-link>
+      </div>
+    </div>
+
+    <div class="home-links text-center mt-4">
+      <span class="mx-4 cursor-pointer hover:text-blue-400" @click="fetchUsers(users.pagination.first['since}'])"
+        >{{ users.pagination && users.pagination.first.rel }}
+      </span>
+      
+      <span
+        class="mx-4 cursor-pointer hover:text-blue-400"
+        @click="fetchUsers(users.pagination.next.since)"
+        >{{ users.pagination && users.pagination.next.rel }}
+      </span>
+    </div>
+  </Layout>
+</template>
+
+<script>
+import axios from "axios"
+
+export default {
+  metaInfo: {
+    title: "Hello, world!"
+  },
+  data() {
+    return {
+      users: []
+    }
+  },
+  methods: {
+    async fetchUsers(since = 0) {
+      try {
+        const { data } = await axios.get(
+          `https://anuar-githubapi-proxy.herokuapp.com/api/users?since=${since}`
+        )
+        this.users = data ? data : []
+      } catch (error) {
+        console.error(error)
+      }
+    }
+  },
+  async mounted() {
+    this.fetchUsers()
+  }
+}
+</script>
+
+<style>
+
+.puff-in-center {
+	-webkit-animation: puff-in-center 0.3s cubic-bezier(0.470, 0.000, 0.745, 0.715) both;
+	        animation: puff-in-center 0.3s cubic-bezier(0.470, 0.000, 0.745, 0.715) both;
+}
+
+@-webkit-keyframes puff-in-center {
+  0% {
+    -webkit-transform: scale(1.2);
+            transform: scale(1.2);
+    -webkit-filter: blur(4px);
+            filter: blur(4px);
+    opacity: 0;
+  }
+  100% {
+    -webkit-transform: scale(1);
+            transform: scale(1);
+    -webkit-filter: blur(0px);
+            filter: blur(0px);
+    opacity: 1;
+  }
+}
+@keyframes puff-in-center {
+  0% {
+    -webkit-transform: scale(1.2);
+            transform: scale(1.2);
+    -webkit-filter: blur(4px);
+            filter: blur(4px);
+    opacity: 0;
+  }
+  100% {
+    -webkit-transform: scale(1);
+            transform: scale(1);
+    -webkit-filter: blur(0px);
+            filter: blur(0px);
+    opacity: 1;
+  }
+}
+
+</style>
---
+++ src/pages/[username].vue
@@ -0,0 +1,123 @@
+<template>
+  <Layout>
+    <div
+      class="grid grid-rows-3 lg:grid-flow-col gap-10 lg:p-8 items-center justify-items-center"
+    >
+      <div class="row-span-3 col-span-3">
+        <div class="user-profile text-center">
+          <div
+            class="rounded-full h-40 w-40 flex items-center justify-center spang-gray-300 mx-auto border-2"
+          >
+            <g-image
+              class="rounded-full"
+              alt="Example image"
+              :src="user.avatar_url"
+              width="100"
+            />
+          </div>
+          <div class="user-id mt-4">
+            <span class="font-semibold">ID</span>: {{ user.id }}
+          </div>
+          <div class="user-login mt-2">
+            <span class="font-semibold">Login</span>:
+            {{ user.login }}
+          </div>
+          <div class="user-profile mt-2">
+            <span class="font-semibold">Profile</span>:
+            <a class="hover:text-blue-500" :href="user.html_url">{{
+              user.login
+            }}</a>
+          </div>
+          <div class="user-created-at mt-2">
+            <span class="font-semibold">Created at</span>:
+            {{ userCreatedDateFormated }}
+          </div>
+        </div>
+      </div>
+      <div class="row-span-3 lg:col-span-2 col-span-3 text-center">
+        <table class="table-fixed mx-auto w-full">
+          <caption class="text-2xl mb-4">
+            Repositories
+          </caption>
+          <thead class="flex w-full border">
+            <tr class="flex w-full mb-2">
+              <th class="w-1/3">ID</th>
+              <th class="w-1/3">Name</th>
+              <th class="w-1/3">URL</th>
+            </tr>
+          </thead>
+          <tbody
+            class="flex flex-col items-center overflow-y-scroll w-full border"
+            style="height: 50vh;"
+          >
+            <tr
+              class="flex w-full mb-2"
+              v-for="repo in repositories"
+              :key="repo.id"
+            >
+              <td class="w-1/3">{{ repo.id }}</td>
+              <td class="w-1/3">{{ repo.name }}</td>
+              <td class="w-1/3">
+                <a class="hover:text-blue-500" :href="repo.html_url"
+                  >Repository</a
+                >
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </Layout>
+</template>
+
+<script>
+import axios from "axios"
+
+export default {
+  metaInfo: {
+    title: "Hello, world!"
+  },
+  data() {
+    return {
+      user: {},
+      repositories: []
+    }
+  },
+  computed: {
+    userCreatedDateFormated() {
+      return this.user.created_at
+        ? new Date(this.user.created_at).toLocaleDateString()
+        : new Date().toLocaleDateString()
+    }
+  },
+  methods: {
+    async fetchUserDetails(username) {
+      try {
+        const { data } = await axios.get(
+          `https://anuar-githubapi-proxy.herokuapp.com/api/users/${username}/details`
+        )
+        this.user = data ? data : {}
+      } catch (error) {
+        console.error(error)
+      }
+    },
+    async fetchUserRepositories(username) {
+      try {
+        const { data } = await axios.get(
+          `https://anuar-githubapi-proxy.herokuapp.com/api/users/${username}/repos`
+        )
+        this.repositories = data ? data : []
+      } catch (error) {
+        console.error(error)
+      }
+    }
+  },
+  async mounted() {
+    const { username } = this.$route.params
+    this.fetchUserDetails(username)
+    this.fetchUserRepositories(username)
+  }
+}
+</script>
+
+<style></style>
